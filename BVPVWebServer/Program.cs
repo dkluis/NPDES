bef7f98fd6c1fd4b25781565b7822864a65a10dc
Dick Kluis <dick@concise-advice.net>
2022-04-05 11:42:06 -0400

    Added MudBlazor to the project

--- BVPVWebServer/Program.cs
+++ BVPVWebServer/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BVPVWebServer.Data;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddMudServices();
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
