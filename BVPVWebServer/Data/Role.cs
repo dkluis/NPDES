9321451fbf50b8be1f38c10ce768c3ea73739875
Dick Kluis <dick@concise-advice.net>
2022-04-06 10:29:06 -0400

    Simplied the Roles Data Service

--- BVPVWebServer/Data/Role.cs
+++
@@ -1,8 +0,0 @@
-namespace BVPVWebServer.Data;
-
-public class Role
-{
-    public string? RoleId { get; set; }
-    public int RoleLevel { get; set; }
-    public bool ReadOnly{ get; set; }
-}
--- BVPVWebServer/Data/RoleService.cs
+++ BVPVWebServer/Data/RoleService.cs
@@ -27,3 +27,10 @@ public class RoleService
         return Task.FromResult(allRoles);
     }
 }
+
+public class Role
+{
+    public string? RoleId { get; set; }
+    public int RoleLevel { get; set; }
+    public bool ReadOnly{ get; set; }
+}
