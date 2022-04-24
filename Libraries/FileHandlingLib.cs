241a68c75475ae67a646099bc5eee4a94ee7a208
Dick Kluis <dick@concise-advice.net>
2022-04-23 22:28:45 -0400

    Upload and FileHandling work

---
+++ Libraries/FileHandlingLib.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Libraries;
+
+public class FileHandling
+{
+    public readonly string ImportPath;
+    public string ProcessedPath;
+    public string LogPath;
+    public string ArchivePath;
+    private readonly AppInfo _appInfo;
+
+    public FileHandling(AppInfo appInfo)
+    {
+        var basePath = new BaseConfig().BasePath;
+        ImportPath = basePath + "Downloads/Imports/";
+        ProcessedPath = basePath + "DownLoads/Processed/";
+        LogPath = new BaseConfig().ConfigPath;
+        ArchivePath = basePath + "Archives";
+        _appInfo = appInfo;
+    }
+
+    public async Task<bool> ImportFile(IBrowserFile file)
+    {
+        try
+        {
+            await using FileStream fs = new($"{ImportPath}/{file.Name}", FileMode.CreateNew);
+            await file.OpenReadStream().CopyToAsync(fs);
+        }
+        catch (Exception e)
+        {
+            _appInfo.TxtFile.Write($"Error: {e.Message}", "FH - ImportFile", 0);
+            return false;
+        }
+        return true;
+    }
+
+    public bool CheckFileExist(IBrowserFile file)
+    {
+        return File.Exists($"{ImportPath}/{file.Name}");
+    }
+
+    public bool ArchiveFile(string from, string to, string file)
+    {
+        File.Move(from, to);
+        return true;
+    }
+}
