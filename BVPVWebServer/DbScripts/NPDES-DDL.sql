b4319038a439a2ce8a2cecf9b435374901031b1e
Dick Kluis <dick@concise-advice.net>
2022-05-11 15:01:48 -0400

    NPDES Schema updated with cascades and column additions

--- BVPVWebServer/DbScripts/NPDES-DDL.sql
+++ BVPVWebServer/DbScripts/NPDES-DDL.sql
@@ -1,9 +1,14 @@
-CREATE TABLE `NPDES`.`Admin-Functions` (
+-- NPDES.`Admin-Functions` definition
+
+CREATE TABLE `Admin-Functions` (
                                    `FunctionID` varchar(30) NOT NULL,
                                    UNIQUE KEY `Functions_UN` (`FunctionID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-Roles` (
+
+-- NPDES.`Admin-Roles` definition
+
+CREATE TABLE `Admin-Roles` (
                                `RoleID` varchar(30) NOT NULL,
                                `RoleLevel` int(11) NOT NULL,
                                `ReadOnly` tinyint(1) NOT NULL,
@@ -11,7 +16,10 @@ CREATE TABLE `NPDES`.`Admin-Roles` (
                                UNIQUE KEY `Roles_UN` (`RoleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-Users` (
+
+-- NPDES.`Admin-Users` definition
+
+CREATE TABLE `Admin-Users` (
                                `UserID` varchar(30) NOT NULL,
                                `Password` varchar(100) NOT NULL,
                                `Salt` varchar(100) DEFAULT NULL,
@@ -19,44 +27,59 @@ CREATE TABLE `NPDES`.`Admin-Users` (
                                UNIQUE KEY `Users_UN` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-Apps` (
+
+-- NPDES.`Admin-Apps` definition
+
+CREATE TABLE `Admin-Apps` (
                               `AppID` varchar(30) NOT NULL,
                               `FunctionID` varchar(30) DEFAULT NULL,
                               `ReportApp` tinyint(1) NOT NULL DEFAULT 1,
                               UNIQUE KEY `Apps_UN` (`AppID`),
                               KEY `Apps_AppID_IDX` (`AppID`) USING BTREE,
                               KEY `Apps_FK_1` (`FunctionID`),
-                              CONSTRAINT `Apps_FK_1` FOREIGN KEY (`FunctionID`) REFERENCES `Admin-Functions` (`FunctionID`)
+                              CONSTRAINT `Apps_FK_Functions` FOREIGN KEY (`FunctionID`) REFERENCES `Admin-Functions` (`FunctionID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-UserAppState` (
+
+-- NPDES.`Admin-UserAppState` definition
+
+CREATE TABLE `Admin-UserAppState` (
                                       `UserID` varchar(30) NOT NULL,
                                       `AppID` varchar(30) NOT NULL,
                                       `Settings` varchar(300) DEFAULT NULL,
                                       UNIQUE KEY `UserAppState_UN` (`UserID`,`AppID`),
                                       KEY `UserAppState_FK_1` (`AppID`),
-                                      CONSTRAINT `UserAppState_FK` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`),
-                                      CONSTRAINT `UserAppState_FK_1` FOREIGN KEY (`AppID`) REFERENCES `Admin-Apps` (`AppID`)
+                                      CONSTRAINT `UserAppState_FK_Users` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`) ON DELETE CASCADE,
+                                      CONSTRAINT `UserAppState_FK_Apps` FOREIGN KEY (`AppID`) REFERENCES `Admin-Apps` (`AppID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-UserRoles` (
+
+-- NPDES.`Admin-UserRoles` definition
+
+CREATE TABLE `Admin-UserRoles` (
                                    `UserID` varchar(30) NOT NULL,
                                    `RoleID` varchar(30) NOT NULL,
                                    UNIQUE KEY `UserRoles_UN` (`UserID`,`RoleID`),
                                    KEY `UserRoles_FK` (`RoleID`),
-                                   CONSTRAINT `UserRoles_FK` FOREIGN KEY (`RoleID`) REFERENCES `Admin-Roles` (`RoleID`) ON DELETE CASCADE,
-                                   CONSTRAINT `UserRoles_FK_1` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+                                   CONSTRAINT `UserRoles_FK_Roles` FOREIGN KEY (`RoleID`) REFERENCES `Admin-Roles` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                   CONSTRAINT `UserRoles_FK_Users` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-UserSystemState` (
+
+-- NPDES.`Admin-UserSystemState` definition
+
+CREATE TABLE `Admin-UserSystemState` (
                                          `UserID` varchar(30) NOT NULL,
                                          `DarkTheme` tinyint(1) NOT NULL DEFAULT 0,
                                          `LastPage` varchar(30) DEFAULT NULL,
                                          UNIQUE KEY `UserSystemState_UN` (`UserID`),
-                                         CONSTRAINT `UserSystemState_FK` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`)
+                                         CONSTRAINT `UserSystemState_FK_Users` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`General-Download` (
+
+-- NPDES.`General-Download` definition
+
+CREATE TABLE `General-Download` (
                                     `UserID` varchar(30) NOT NULL,
                                     `FileName` varchar(100) NOT NULL,
                                     `OriginalFileName` varchar(100) DEFAULT NULL,
@@ -66,58 +89,22 @@ CREATE TABLE `NPDES`.`General-Download` (
                                     `ValidateUserID` varchar(30) DEFAULT NULL,
                                     `ProcessedDateTime` datetime(3) DEFAULT NULL,
                                     `ProcessUserID` varchar(30) DEFAULT NULL,
-                                    `ArchiveDataTime` datetime(3) DEFAULT NULL,
+                                    `ArchivedDateTime` datetime(3) DEFAULT NULL,
                                     `ArchiveUserID` varchar(30) DEFAULT NULL,
                                     PRIMARY KEY (`UserID`,`FileName`),
                                     KEY `General_Download_FK` (`FunctionID`),
-                                    CONSTRAINT `General_Download_FK` FOREIGN KEY (`FunctionID`) REFERENCES `Admin-Functions` (`FunctionID`),
-                                    CONSTRAINT `General_Download_FK_1` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`)
+                                    CONSTRAINT `General_Download_FK_Functions` FOREIGN KEY (`FunctionID`) REFERENCES `Admin-Functions` (`FunctionID`),
+                                    CONSTRAINT `General_Download_FK_Users` FOREIGN KEY (`UserID`) REFERENCES `Admin-Users` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `NPDES`.`Admin-AppRoles` (
+
+-- NPDES.`Admin-AppRoles` definition
+
+CREATE TABLE `Admin-AppRoles` (
                                   `AppID` varchar(30) NOT NULL,
                                   `RoleID` varchar(30) NOT NULL,
                                   UNIQUE KEY `AppRoles_UN` (`AppID`,`RoleID`),
                                   KEY `AppRoles_FK` (`RoleID`),
-                                  CONSTRAINT `AppRoles_FK` FOREIGN KEY (`RoleID`) REFERENCES `Admin-Roles` (`RoleID`) ON DELETE CASCADE,
-                                  CONSTRAINT `AppRoles_FK_1` FOREIGN KEY (`AppID`) REFERENCES `Admin-Apps` (`AppID`) ON DELETE CASCADE
+                                  CONSTRAINT `AppRoles_FK_Roles` FOREIGN KEY (`RoleID`) REFERENCES `Admin-Roles` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                  CONSTRAINT `AppRoles_FK_Apps` FOREIGN KEY (`AppID`) REFERENCES `Admin-Apps` (`AppID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE OR REPLACE
-    ALGORITHM = UNDEFINED VIEW `NPDES`.`Admin-AppsByUserView` AS
-select
-    `ur`.`UserID` AS `User`,
-    `ur`.`RoleID` AS `Role`,
-    `r`.`ReadOnly` AS `ReadOnly`,
-    `r`.`RoleLevel` AS `Role Level`,
-    `r`.`Enabled` AS `Role Enabled`,
-    `ar`.`AppID` AS `App`,
-    `a`.`ReportApp` AS `Report`,
-    `a`.`FunctionID` AS `Function`
-from
-    (((`NPDES`.`Admin-UserRoles` `ur`
-        join `NPDES`.`Admin-Roles` `r` on
-        (`ur`.`RoleID` = `r`.`RoleID`))
-        join `NPDES`.`Admin-AppRoles` `ar` on
-        (`ur`.`RoleID` = `ar`.`RoleID`))
-        join `NPDES`.`Admin-Apps` `a` on
-        (`ar`.`AppID` = `a`.`AppID`))
-order by
-    `ur`.`UserID`,
-    `r`.`RoleLevel`,
-    `ar`.`AppID`;
-
-CREATE OR REPLACE
-    ALGORITHM = UNDEFINED VIEW `NPDES`.`Admin-AppsWithoutRoles` AS
-select
-    `a`.`AppID` AS `App`,
-    `a`.`ReportApp` AS `Report`,
-    `a`.`FunctionID` AS `Function`,
-    `ar`.`RoleID` AS `Role`
-from
-    (`NPDES`.`Admin-Apps` `a`
-        left join `NPDES`.`Admin-AppRoles` `ar` on
-        (`ar`.`AppID` = `a`.`AppID`
-            and `ar`.`RoleID` <> 'SuperAdmin'))
-where
-    `ar`.`RoleID` is null;
